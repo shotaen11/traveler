b9f247d19567f5d0aad451ab21f0d1224d640fc1
shotaen11 <180824926+shotaen11@users.noreply.github.com>
2024-10-12 20:52:02 +0900

    閲覧・投稿詳細ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,17 @@ class PostsController < ApplicationController
   def create
     post = Post.new(post_params)
     post.save
-    redirect_to '/top'
+    redirect_to post_path(post.id)
   end
 
   def index
     @posts = Post.all 
   end
-  
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
   private
   def post_params
     params.require(:post).permit(:location, :text)
