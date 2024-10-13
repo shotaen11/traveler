929fb8b7ae4c3de75a8935907153ce8820b85360
shotaen11 <180824926+shotaen11@users.noreply.github.com>
2024-10-13 10:43:30 +0900

    画像投稿機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_12_102516) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_13_011139) do
   create_table "posts", force: :cascade do |t|
     t.string "location"
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "image_id"
   end
 
 end
