8d4677c809f10d3e13b7a161af6abe0bf93b3f79
shotaen11 <180824926+shotaen11@users.noreply.github.com>
2024-10-19 22:11:39 +0900

    ユーザー詳細・一覧ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only:[:index, :show]
   root :to => 'homes#top'
   post 'posts' => 'posts#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_19_105805) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_19_114757) do
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.integer "user_id"
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_19_105805) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.string "image_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
