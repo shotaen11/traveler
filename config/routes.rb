c47e45ab9fd36376383dd3e7b3e3eebe70ca8957
shotaen11 <180824926+shotaen11@users.noreply.github.com>
2024-10-24 23:09:27 +0900

    フォロー機能コントローラ作成まで

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,11 @@ Rails.application.routes.draw do
     resources :comments, only:[:create, :destroy]
     resource :favorites, only:[:create, :destroy]
   end
+
+  resources :users, only:[:index, :show, :edit, :update] do
+    member do
+      get :follows, :followers
+    end
+    resource :relationships, only: [:create, :destroy]
+  end
 end
