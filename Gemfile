bd981ffd764c1388a1f23159ef60b26e2ef40bb8
shotaen11 <180824926+shotaen11@users.noreply.github.com>
2024-10-27 13:20:51 +0900

    下書き機能_日本語化まで

--- Gemfile
+++ Gemfile
@@ -78,3 +78,5 @@ gem "sassc-rails"
 gem 'devise'
 
 gem 'kaminari'
+
+gem 'enum_help'
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,8 @@ class Post < ApplicationRecord
     validates :location, presence: true, length: { maximum: 15 }
     validates :text, presence: true, length: { maximum: 195 }
     validates :image, presence: true
+
+    enum status: { published: 0, draft: 1 }
     
     def favorited_by?(user)
         favorites.where(user_id: user.id).exists?
